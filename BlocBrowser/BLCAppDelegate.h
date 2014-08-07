afe6b9124c6e630ac7d3706a2797040ffa7e1786
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-08-07 16:32:57 -0400

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by Trevor Ahlert on 8/7/14.
+//  Copyright (c) 2014 Trevor Ahlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
