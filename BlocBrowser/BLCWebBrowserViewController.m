0ee2c3f2f308101de82c0bb26d10a681e6886398
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-08-12 22:28:55 -0400

    Funcional web browser

--- BlocBrowser/BLCWebBrowserViewController.m
+++ BlocBrowser/BLCWebBrowserViewController.m
@@ -36,7 +36,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString (@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString (@"Enter Website URL or Search", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
@@ -117,10 +117,18 @@
     NSString *URLString = textField.text;
     
     NSURL *URL = [NSURL URLWithString:URLString];
-    
+    NSRange whiteSpaceRange = [URLString rangeOfCharacterFromSet: [NSCharacterSet whitespaceCharacterSet]];
+    NSString *searchString = [URLString stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+
     if (!URL.scheme) {
         // The user didn't type http: or https:
-        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+                if (whiteSpaceRange.location != NSNotFound)
+        {
+            URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://google.com/search?q=%@", searchString]];
+        }
+        
+        
+   else    URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
     }
     
     if (URL) {
